c589e3d39dfd5bcf5e8d5f99f4f27e4c313d8a65
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-12-25 12:54:29 +0100

    Init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" width="100">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
