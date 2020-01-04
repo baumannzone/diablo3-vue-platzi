ad33e1e50b56f3420048c652f235d1c8072246a9
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-04 18:17:32 +0100

    Babel eslint version 7

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,27 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
+const routerOptions = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'Home'
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'About'
   }
 ]
 
-const router = new VueRouter({
+const routes = routerOptions.map(r => {
+  return {
+    ...r,
+    component: () => import(`@/views/${r.name}/Index.vue`)
+  }
+})
+
+const router = new Router({
   routes
 })
 
--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" width="100">
+    <img alt="Vue logo" src="../../assets/logo.png">
     <HelloWorld/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
