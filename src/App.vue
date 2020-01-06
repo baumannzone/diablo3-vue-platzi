04145201f2dca36036f00ebc525ed496143964d1
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-06 16:48:21 +0100

    Loading Comp

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <template v-if="isLoading">
+      <div class="d-flex justify-content-center mt-4">
+        <LoadingIcon/>
+      </div>
+    </template>
+    <template v-else>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view/>
+    </template>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import LoadingIcon from '@/components/Loading'
+
+export default {
+  name: 'App',
+  components: {
+    LoadingIcon
+  },
+  computed: {
+    ...mapState({
+      isLoading: 'isLoading'
+    })
+  }
+}
+</script>
 
 <style lang="stylus">
 #app
---
+++ src/components/Loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d-flex">
+    <div class="loading-spinner">
+      <font-awesome-icon icon="spinner" class="fa-spin fa-3x text-info"/>
+      <h3 class="mt-5">Loading...</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {
+    size: {
+      type: Number,
+      required: false,
+      default: 80
+    }
+  }
+}
+</script>
