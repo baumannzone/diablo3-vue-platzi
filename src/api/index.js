d4410eec856450a406fa9fcf00652f7453a58db9
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-08 19:47:59 +0100

    Update API functions

--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,12 @@ import store from '../store/index'
 
 // {region} is one of: ['us', 'eu', 'kr', 'tw']
 // More info: https://develop.battle.net/documentation/guides/regionality-and-apis
-const region = 'eu'
+const { region } = store.state.config
+const { locale } = store.state.config
 
-// const lang = 'es_ES'
-// const playerTag = 'SuperRambo#2613'
+const account = store.getters.account()
+// const account = 'BazzMasta-2491'
+
 const API_URL = `https://${region}.api.blizzard.com/`
 
 /**
@@ -21,6 +23,21 @@ function listSeason () {
   return get(`${API_URL}${resource}`, { params })
 }
 
+/**
+ * Returns the specified account profile.
+ * @returns {*}
+ */
+function getAccount () {
+  const resource = `d3/profile/${account}/`
+
+  const params = {
+    'access_token': store.state.accessToken,
+    locale
+  }
+  return get(`${API_URL}${resource}`, { params })
+}
+
 export {
+  getAccount,
   listSeason
 }
