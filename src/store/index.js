e2e93aa703da6ca0a7c231bd214987097ec60b22
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-08 16:37:25 +0100

    Update store with config

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,12 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     isLoading: false,
-    accessToken: null
+    accessToken: null,
+    config: {
+      battleTag: null,
+      region: null,
+      locale: null
+    }
   },
   mutations: {
     SET_ACCESS_TOKEN (state, payload) {
@@ -14,6 +19,10 @@ export default new Vuex.Store({
     },
     SET_LOADING (state, payload) {
       state.isLoading = payload
+    },
+    SET_CONFIG (state, payload) {
+      // Merge 2 items
+      state.config = { ...state.config, ...payload }
     }
   },
   actions: {
@@ -23,6 +32,19 @@ export default new Vuex.Store({
     setLoading ({ commit }, payload) {
       commit('SET_LOADING', payload)
     }
+  },
+  getters: {
+    selectedRegion: (state) => state.config.region,
+    selectedLocale: (state) => state.config.locale,
+    playerName: (state) => state.config.battleTag.split('#')[0],
+    playerNumber: (state) => state.config.battleTag.split('#')[1],
+    /**
+     * Return the BattleTag for the account to retrieve, ready to use in the API
+     * Replaces `#` for `-`
+     * @param state
+     * @returns {*}
+     */
+    account: (state) => state.config.battleTag.replace('#', '-')
   },
   modules: {
   }
