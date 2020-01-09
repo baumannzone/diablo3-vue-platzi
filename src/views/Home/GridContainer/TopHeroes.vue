27bbeaabc586c403ed6d2f4cb022dd9f5ecd3ded
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-09 19:36:22 +0100

    Top heroes comp

---
+++ src/views/Home/GridContainer/TopHeroes.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="top-heroes">
+    <h2 class="my-4">Top Heroes</h2>
+    <b-row>
+      <b-col cols="4" v-for="hero in heroes" :key="hero.id">
+        <TopHero :hero="hero"/>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import TopHero from './TopHero'
+
+export default {
+  name: 'TopHeroes',
+  components: { TopHero },
+  props: {
+    heroes: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
