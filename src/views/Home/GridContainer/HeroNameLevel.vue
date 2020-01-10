3156598a9dc26cf0427cc6d75927e90456380c2a
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-10 13:58:50 +0100

    HeroNameLevel comp

---
+++ src/views/Home/GridContainer/HeroNameLevel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hero-name-level">
+    <span class="font-weight-bold"> {{ classToName(hero.name) }} </span>
+    <span>·</span>
+    <span> {{ hero.level }} </span>
+  </div>
+</template>
+
+<script>
+import heroName from '@/mixins/heroName.js'
+
+export default {
+  name: 'HeroNameLevel',
+  mixins: [heroName],
+  props: {
+    hero: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
