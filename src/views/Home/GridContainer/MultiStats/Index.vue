bcd165f2dac73a45b5e5000926425203ac5e3c46
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-10 21:42:50 +0100

    Create Stats comp

---
+++ src/views/Home/GridContainer/MultiStats/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="multi-stats pl-lg-4">
+    <h2 class="font-diablo my-4">Stats</h2>
+    <div class="stats d-flex flex-column flex-md-row flex-lg-column bg-dark p-3">
+      <SingleStat ico-name="skull"/>
+      <SingleStat ico-name="crown"/>
+      <SingleStat ico-name="award"/>
+    </div>
+    <div class="time-played"></div>
+  </div>
+</template>
+
+<script>
+import SingleStat from './SingleStat'
+
+export default {
+  name: 'MultiStats',
+  components: { SingleStat }
+}
+</script>
---
+++ src/views/Home/GridContainer/MultiStats/SingleStat.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sigle-stat w-100 mb-3">
+    <b-card class="text-body">
+      <div class="d-flex align-content-center">
+        <div class="mr-3">
+          <font-awesome-icon :icon="icoName" class="fa-2x" />
+        </div>
+        <div>
+          Numero + Texto
+        </div>
+      </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleStat',
+  props: {
+    icoName: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
