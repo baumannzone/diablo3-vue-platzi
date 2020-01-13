96644d8240095d8a7414b15d6c8e92d7e369b804
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-13 20:33:23 +0100

    Create heroClasses.js

---
+++ src/utils/heroClasses.js
@@ -0,0 +1,11 @@
+const classes = {
+  barbarian: 'Barbarian',
+  crusader: 'Crusader',
+  'demon-hunter': 'Demon Hunter',
+  monk: 'Monk',
+  necromancer: 'Necromancer',
+  'witch-doctor': 'Witch Doctor',
+  wizard: 'Wizard'
+}
+
+export default classes
