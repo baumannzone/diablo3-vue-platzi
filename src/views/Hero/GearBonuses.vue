49e38c8b29fb885dc4285448b6949cd2a28e3738
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-15 22:58:48 +0100

    Update GearBonuses.vue

--- src/views/Hero/GearBonuses.vue
+++ src/views/Hero/GearBonuses.vue
@@ -1,19 +1,60 @@
 <template>
-  <b-col>
-    <h2 class="font-diablo">Gear Bonuses</h2>
+  <div>
+    <h2 class="font-diablo">Attributes</h2>
 
     <hr class="bg-white">
 
-  </b-col>
+    <div class="attributes">
+
+      <div class="core">
+        <ul v-for="attr in coreAttributes" :key="attr.name">
+          <li>{{ attr }}</li>
+        </ul>
+      </div>
+
+      <hr>
+
+      <div class="secondary">
+        <ul v-for="attr in secondaryAttributes" :key="attr.name">
+          <li>{{ attr }}</li>
+        </ul>
+      </div>
+
+    </div>
+
+    <hr>
+
+    <div class="resources">
+      <ul v-for="res in resources" :key="res.name">
+        <li>{{ res }}</li>
+      </ul>
+    </div>
+
+  </div>
 </template>
 
 <script>
+const coreAttributes = ['strength', 'dexterity', 'vitality', 'intelligence']
+const secondaryAttributes = ['damage', 'toughness', 'healing']
+const resources = ['life', 'primaryResource']
+
 export default {
   name: 'GearBonuses',
   props: {
     stats: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    coreAttributes () {
+      return coreAttributes.map(item => ({ name: item, val: this.stats[item] }))
+    },
+    secondaryAttributes () {
+      return secondaryAttributes.map(item => ({ name: item, val: this.stats[item] }))
+    },
+    resources () {
+      return resources.map(item => ({ name: item, val: this.stats[item] }))
     }
   }
 }
