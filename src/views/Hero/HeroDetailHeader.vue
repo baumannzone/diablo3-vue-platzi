f7a9d4776f922a3bb4dd1061e44c313bd911c6af
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-15 14:42:27 +0100

    Create HeroDetailHeader.vue

---
+++ src/views/Hero/HeroDetailHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-row class="my-5">
+    <b-col cols="12">
+      <div class="text-center">
+        <h1 class="font-diablo text-truncate">Aourus</h1>
+        <div>
+          <small>70 · (2637) · Crusader - Seasonal - Hardcore </small>
+        </div>
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'HeroDetailHeader'
+}
+</script>
