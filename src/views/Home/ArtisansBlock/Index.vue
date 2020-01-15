00894ca80a6717f944561696f309d530ab0801ec
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-15 11:14:01 +0100

    Update Index.vue

--- src/views/Home/ArtisansBlock/Index.vue
+++ src/views/Home/ArtisansBlock/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="artisan-list">
     <hr class="bg-light my-5">
-    <h3 class="font-diablo">Artisans</h3>
+    <h3 class="font-diablo mb-4">Artisans</h3>
     <b-row>
       <b-col lg="4" v-for="artisan in artisansInfo" :key="artisan.name">
         <ArtisanItem :artisan="artisan"/>
@@ -29,6 +29,7 @@ export default {
         {
           name: 'blacksmith',
           icon: 'hammer',
+          emoji: '⚒️',
           color: '#ffb74d',
           normal: data.blacksmith,
           hardcore: data.blacksmithHardcore
@@ -36,6 +37,7 @@ export default {
         {
           name: 'jeweler',
           icon: 'gem',
+          emoji: '💎',
           color: '#4dd0e1',
           normal: data.jeweler,
           hardcore: data.jewelerHardcore
@@ -43,6 +45,7 @@ export default {
         {
           name: 'mystic',
           icon: 'hat-wizard',
+          emoji: '🔮',
           color: '#ba68c8',
           normal: data.mystic,
           hardcore: data.mysticHardcore
