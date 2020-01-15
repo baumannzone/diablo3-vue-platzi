363507659842fb87e4c695b64b4028fee276c621
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-15 12:34:15 +0100

    Create Index.vue

---
+++ src/views/Hero/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hero-view">
+    <b-breadcrumb :items="items" class="bg-transparent"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroView',
+  data () {
+    return {
+      items: [
+        {
+          text: 'SuperRambo#2613',
+          to: { name: 'Home' }
+        },
+        {
+          text: '[CR] ElMicroYyo',
+          active: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
