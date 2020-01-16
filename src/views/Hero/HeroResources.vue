c2ddf8935861b2fc3e76f57960d36b4270a1bf97
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-16 14:59:15 +0100

    WIP

---
+++ src/views/Hero/HeroResources.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    {{ resources.resources }}
+    <div class="resource">
+      <div class="resource-icon resource-life"/>
+      <span> {{ resources.resources.life.val }} </span>
+    </div>
+    <div class="resource">
+      <div class="resource-icon" :class="classResourceName"/>
+      <span> {{ resources.resources.primaryResource.val }} </span>
+      <template v-if="hasSecondaryResource">
+        <span class="mx-2 text-muted">/</span>
+        <span> {{ resources.resources.secondaryResource.val }} </span>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import resources from '@/mixins/resources'
+
+export default {
+  name: 'HeroResources',
+  mixins: [resources],
+  props: {
+    resources: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    classResourceName () {
+      return `resource-${this.resourceName(this.resources.classSlug)}`
+    },
+    hasSecondaryResource () {
+      return this.resources.classSlug === 'demon-hunter'
+    }
+  }
+}
+</script>
