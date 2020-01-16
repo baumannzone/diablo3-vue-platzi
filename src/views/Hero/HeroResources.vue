4fdf27acaa1c1abee15a38d946cf8fb838c9584a
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-16 16:50:33 +0100

    Update HeroResources.vue

--- src/views/Hero/HeroResources.vue
+++ src/views/Hero/HeroResources.vue
@@ -1,27 +1,52 @@
 <template>
   <div>
-    {{ resources.resources }}
     <div class="resource">
-      <div class="resource-icon resource-life"/>
-      <span> {{ resources.resources.life.val }} </span>
+      <div class="d-flex justify-content-start align-items-center">
+        <div class="resource-icon resource-life"/>
+        <div class="ml-3 text-uppercase name-text">
+          <span>{{ resources.resources.life.name }}</span>
+        </div>
+        <div class="ml-3">
+          <span class="text-monospace"> {{ resources.resources.life.val | formatNumber }} </span>
+        </div>
+      </div>
+
     </div>
+
+    <hr>
+
     <div class="resource">
-      <div class="resource-icon" :class="classResourceName"/>
-      <span> {{ resources.resources.primaryResource.val }} </span>
-      <template v-if="hasSecondaryResource">
-        <span class="mx-2 text-muted">/</span>
-        <span> {{ resources.resources.secondaryResource.val }} </span>
-      </template>
+      <div class="d-flex justify-content-start align-items-center">
+        <div class="resource-icon" :class="classResourceName"/>
+        <div class="ml-3 text-uppercase name-text" :class="'resource-name-' + resources.classSlug">
+          <span>{{ displayResourceName }}</span>
+        </div>
+        <div class="ml-3">
+          <span class="text-monospace">
+            {{ resources.resources.primaryResource.val | formatNumber }}
+          <template v-if="hasSecondaryResource">
+            <span class="mx-0 text-muted">/</span>
+            <span> {{ resources.resources.secondaryResource.val | formatNumber }} </span>
+          </template>
+          </span>
+        </div>
+      </div>
+
     </div>
+
   </div>
 </template>
 
 <script>
 import resources from '@/mixins/resources'
+import { formatNumber } from '@/filters/numeral'
 
 export default {
   name: 'HeroResources',
   mixins: [resources],
+  filters: {
+    formatNumber
+  },
   props: {
     resources: {
       required: true,
@@ -30,7 +55,10 @@ export default {
   },
   computed: {
     classResourceName () {
-      return `resource-${this.resourceName(this.resources.classSlug)}`
+      return `resource-${this.resourceClassName(this.resources.classSlug)}`
+    },
+    displayResourceName () {
+      return this.resourceDisplayName(this.resources.classSlug)
     },
     hasSecondaryResource () {
       return this.resources.classSlug === 'demon-hunter'
@@ -38,3 +66,17 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+  .resource
+    .name-text
+      color #efb45d
+
+    .resource-name-demon-hunter
+      width auto
+
+  @media (min-width: 992px)
+    .resource
+      .resource-name-demon-hunter
+        width 100px
+</style>
