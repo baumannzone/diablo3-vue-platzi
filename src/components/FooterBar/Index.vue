25ad178bdb2afe848f2f530b0e3f06a6434db256
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-22 23:51:55 +0100

    Create Index.vue

---
+++ src/components/FooterBar/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer-bar text-center">
+    <p class="text-muted">Powered by</p>
+    <ul class="list-inline">
+      <li class="list-inline-item">
+        <a href="#">Vuejs</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#">Vuejs</a>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterBar'
+}
+</script>
+
+<style scoped>
+
+</style>
