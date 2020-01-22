a6952105ed1a72aa10c4fa8a1d3da26beb9f9f17
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-22 18:29:48 +0100

    Create BaseLoading.vue

---
+++ src/components/BaseLoading.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="loading-wrapper text-center py-5">
+    <div class="spinner-wrapper w-100">
+      <b-spinner class="spinner" label="Loading..." type="grow" variant="info"/>
+    </div>
+    <h1 class="my-5">Loading...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {
+    size: {
+      type: Number,
+      required: false,
+      default: 80
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .spinner-wrapper
+    .spinner
+      width 4rem
+      height 4rem
+
+</style>
