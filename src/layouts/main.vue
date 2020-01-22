5b5ef40bab61879749eafed1f3f8a5e21679a253
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-22 18:29:09 +0100

    Create main.vue
    Layout

---
+++ src/layouts/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <HeaderBar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/HeaderBar/Index'
+
+export default {
+  name: 'MainLayout',
+  components: {
+    HeaderBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
