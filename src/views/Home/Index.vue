aa3a07d62902a9acf6e31c53a9a23e1cf7a38522
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-22 18:47:27 +0100

    Update Index.vue

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    <h1>home</h1>
+  <div class="home-view">
+
+    <h1>Profile Finder</h1>
+
+    <MainForm/>
+
   </div>
 </template>
 
 <script>
+import MainForm from './MainForm'
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: { MainForm }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+  .home-view
+    color white
 
 </style>
