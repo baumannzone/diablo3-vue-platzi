f1f2f8cf68b4da1be280b1ebf0e0d7434f466857
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-22 18:32:59 +0100

    Update BrandTitle.vue

--- src/components/HeaderBar/BrandTitle.vue
+++ src/components/HeaderBar/BrandTitle.vue
@@ -1,28 +1,11 @@
 <template>
-  <b-navbar-brand @click="goHome()" class="hover-cursor-pointer">
-    <span class="mr-1 font-diablo">{{ playerName }}</span>
-    <small class="text-muted">
-      <b>#{{playerNumber}}</b>
-    </small>
+  <b-navbar-brand :to="{ name: 'Home' }" class="hover-cursor-pointer">
+    <img src="@/assets/img/diablo-iii.svg" alt="D3" width="30">
   </b-navbar-brand>
 </template>
+
 <script>
 export default {
-  name: 'BrandTitle',
-  props: {
-    playerName: {
-      required: true,
-      type: String
-    },
-    playerNumber: {
-      required: true,
-      type: String
-    }
-  },
-  methods: {
-    goHome () {
-      this.$router.push({ name: 'Profile' })
-    }
-  }
+  name: 'BrandTitle'
 }
 </script>
