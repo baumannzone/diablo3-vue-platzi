9c513cd1c7a81aa1d1600510f1f79936cfab81f6
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-23 18:10:28 +0100

    Footer :bowtie:

--- src/components/FooterBar/Index.vue
+++ src/components/FooterBar/Index.vue
@@ -2,23 +2,45 @@
   <footer class="footer-bar text-center">
     <p class="text-muted">Powered by</p>
     <ul class="list-inline">
-      <li class="list-inline-item">
-        <a href="#">
-          <font-awesome-icon :icon="['fab', 'vuejs']" />
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a href="#">
-          <font-awesome-icon :icon="['fab', 'bootstrap']" />
+      <li class="list-inline-item" v-for="(icon, idx) in icons" :key="idx">
+        <a :href="icon.href" target="_blank">
+          <font-awesome-icon :icon="icon.classes" class="fa-lg" :style="{'color': icon.color}"/>
         </a>
       </li>
     </ul>
+
+    <hr>
+
+    <p class="text-muted">
+      <small>
+        Made by:
+        <a href="https://twitter.com/baumannzone" target="_blank">Jorge Baumann</a>
+        for
+        <a href="https://platzi.com/" target="_blank">Platzi</a>
+      </small>
+    </p>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'FooterBar'
+  name: 'FooterBar',
+  data () {
+    return {
+      icons: [
+        {
+          classes: [ 'fab', 'vuejs' ],
+          color: '#4fc08d',
+          href: 'https://vue.js.org/'
+        },
+        {
+          classes: [ 'fab', 'bootstrap' ],
+          color: '#7952b3',
+          href: 'https://bootstrap-vue.js.org/'
+        }
+      ]
+    }
+  }
 }
 </script>
 
