87ed86139dca33f2dc1baa87a014243029446fc1
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-23 20:14:59 +0100

    Update regions.js

--- src/utils/regions.js
+++ src/utils/regions.js
@@ -1,3 +1,5 @@
+// More info: https://develop.battle.net/documentation/guides/regionality-and-apis
+
 const regions = ['us', 'eu', 'kr', 'tw']
 const defaultLocales = ['en_US', 'en_GB', 'ko_KR', 'zh_TW']
 
