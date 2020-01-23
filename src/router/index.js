24b62300d9f56783c385252eb0fe1c7f3641243d
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-24 00:08:54 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 const routOptions = [
   { path: '/', name: 'Home' },
   { path: '/:region/profile/:battleTag', name: 'Profile' },
-  { path: '/:region/profile/:battleTag/hero/heroId', name: 'Hero' },
+  { path: '/:region/profile/:battleTag/hero/:heroId', name: 'Hero' },
   { path: '/about', name: 'About' }
 ]
 
