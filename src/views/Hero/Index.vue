c537a71eff9e5f5d70189e706adbc4b6d29f33c6
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-24 16:30:10 +0100

    Update Index.vue

--- src/views/Hero/Index.vue
+++ src/views/Hero/Index.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="hero-view">
 
-    <HeroDetailHeader :detail="detailHeader"/>
+    <template v-if="hero">
 
-    <b-row>
-      <b-col md="12" lg="8" order-lg="2">
-        <HeroItems :items="items"/>
-      </b-col>
+      <HeroDetailHeader :detail="detailHeader"/>
 
-      <b-col md="12" lg="4" order-lg="1">
-        <GearBonuses :stats="detailStats"/>
-        <Skills :skills="hero.skills"/>
-      </b-col>
+      <b-row>
+        <b-col md="12" lg="8" order-lg="2">
+          <HeroItems :items="items"/>
+        </b-col>
 
-    </b-row>
+        <b-col md="12" lg="4" order-lg="1">
+          <GearBonuses :stats="detailStats"/>
+          <Skills :skills="hero.skills"/>
+        </b-col>
+
+      </b-row>
+
+    </template>
 
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
-// import h from '@/api/heroDetail.json'
-import items from '@/api/heroItems.json'
+import { getApiHero, getApiDetailedHeroItems } from '@/api/search'
 
 import HeroDetailHeader from './HeroDetailHeader'
 import GearBonuses from './GearBonuses/Index'
@@ -34,11 +35,11 @@ export default {
   components: { Skills, GearBonuses, HeroDetailHeader, HeroItems },
   data () {
     return {
-      items
+      hero: null,
+      items: null
     }
   },
   computed: {
-    ...mapState('profile', ['hero']),
     detailHeader () {
       const { name, class: classSlug, gender, level, hardcore, seasonal, paragonLevel, alive, seasonCreated } = this.hero
       return {
@@ -56,6 +57,29 @@ export default {
     detailStats () {
       return { ...this.hero.stats, classSlug: this.hero.class }
     }
+  },
+  created () {
+    const { region, battleTag: account, heroId } = this.$route.params
+
+    getApiHero({ region, account, heroId })
+      .then(({ data }) => {
+        console.log(data)
+        this.hero = data
+      })
+      .catch((err) => {
+        this.hero = null
+        console.log(err)
+      })
+
+    getApiDetailedHeroItems({ region, account, heroId })
+      .then(({ data }) => {
+        console.log(data)
+        this.items = data
+      })
+      .catch((err) => {
+        this.items = null
+        console.log(err)
+      })
   }
 }
 </script>
