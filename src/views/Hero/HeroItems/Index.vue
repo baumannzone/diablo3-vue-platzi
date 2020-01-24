fa94961eda59f38f14df8cc5452dd30b60c98a01
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-24 16:28:40 +0100

    Update Index.vue

--- src/views/Hero/HeroItems/Index.vue
+++ src/views/Hero/HeroItems/Index.vue
@@ -4,69 +4,75 @@
 
     <hr class="bg-white">
 
-    <b-row>
-      <b-col cols="4" offset="4">
-        <ItemDetail :item="items.head"/>
-      </b-col>
-    </b-row>
+    <template v-if="items === null">
+      <h3>Loading...</h3>
+    </template>
+    <template v-else>
+      <b-row>
+        <b-col cols="4" offset="4">
+          <ItemDetail :item="items.head"/>
+        </b-col>
+      </b-row>
 
-    <hr>
+      <hr>
 
-    <b-row>
-      <b-col>
-        <ItemDetail :item="items.shoulders"/>
-      </b-col>
-      <b-col>
-        <ItemDetail :item="items.torso"/>
-      </b-col>
-      <b-col>
-        <ItemDetail :item="items.neck"/>
-      </b-col>
-    </b-row>
+      <b-row>
+        <b-col>
+          <ItemDetail :item="items.shoulders"/>
+        </b-col>
+        <b-col>
+          <ItemDetail :item="items.torso"/>
+        </b-col>
+        <b-col>
+          <ItemDetail :item="items.neck"/>
+        </b-col>
+      </b-row>
 
-    <hr>
+      <hr>
 
-    <b-row>
-      <b-col>
-        <ItemDetail :item="items.hands"/>
-      </b-col>
-      <b-col>
-        <ItemDetail :item="items.waist"/>
-      </b-col>
-      <b-col>
-        <ItemDetail :item="items.bracers"/>
-      </b-col>
-    </b-row>
+      <b-row>
+        <b-col>
+          <ItemDetail :item="items.hands"/>
+        </b-col>
+        <b-col>
+          <ItemDetail :item="items.waist"/>
+        </b-col>
+        <b-col>
+          <ItemDetail :item="items.bracers"/>
+        </b-col>
+      </b-row>
 
-    <hr>
+      <hr>
 
-    <b-row>
-      <b-col>
-        <ItemDetail :item="items.leftFinger"/>
-      </b-col>
-      <b-col>
-        <ItemDetail :item="items.legs"/>
-      </b-col>
-      <b-col>
-        <ItemDetail :item="items.rightFinger"/>
-      </b-col>
-    </b-row>
+      <b-row>
+        <b-col>
+          <ItemDetail :item="items.leftFinger"/>
+        </b-col>
+        <b-col>
+          <ItemDetail :item="items.legs"/>
+        </b-col>
+        <b-col>
+          <ItemDetail :item="items.rightFinger"/>
+        </b-col>
+      </b-row>
 
-    <hr>
+      <hr>
 
-    <b-row>
-      <b-col>
-        <ItemDetail :item="items.mainHand"/>
-      </b-col>
-      <b-col>
-        <ItemDetail :item="items.feet"/>
-      </b-col>
-      <b-col>
-        <ItemDetail :item="items.offHand"/>
-      </b-col>
-    </b-row>
+      <b-row>
+        <b-col>
+          <ItemDetail :item="items.mainHand"/>
+        </b-col>
+        <b-col>
+          <ItemDetail :item="items.feet"/>
+        </b-col>
+        <b-col>
+          <ItemDetail :item="items.offHand"/>
+        </b-col>
+      </b-row>
+    </template>
 
   </section>
+
 </template>
 
 <script>
