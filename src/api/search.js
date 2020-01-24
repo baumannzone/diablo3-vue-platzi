a01585dfe78c650d13572dd044f5efb94a3e3b9a
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-24 16:24:25 +0100

    Update search.js

--- src/api/search.js
+++ src/api/search.js
@@ -2,11 +2,13 @@ import { get } from 'axios'
 import store from '../store/index'
 import { locales } from '../utils/regions'
 
+// https://{region}.api.blizzard.com, where {region} is one of us | eu | kr | tw
 const protocol = 'https://'
 const host = '.api.blizzard.com/'
 
 /**
  * Returns the specified account profile.
+ * GET – /d3/profile/{account}
  * @param region {String}
  * @param account {String}
  * @returns {Promise}
@@ -24,6 +26,50 @@ function getApiAccount ({ region, account }) {
   return get(`${API_URL}`, { params })
 }
 
+/**
+ * Returns a single hero
+ * GET – /d3/profile/{account}/hero/{heroId}
+ * @param region {String}
+ * @param account {String}
+ * @param heroId {String}
+ * @returns {Promise}
+ */
+function getApiHero ({ region, account, heroId }) {
+  const resource = `d3/profile/${account}/hero/${heroId}`
+  const API_URL = `${protocol}${region}${host}${resource}`
+  const locale = locales[region]
+
+  const params = {
+    'access_token': store.state.oauth.accessToken,
+    locale
+  }
+
+  return get(`${API_URL}`, { params })
+}
+
+/**
+ * Returns a list of items for the specified hero.
+ * GET – /d3/profile/{account}/hero/{heroId}/items
+ * @param region {String}
+ * @param account {String}
+ * @param heroId {String}
+ * @returns {Promise}
+ */
+function getApiDetailedHeroItems ({ region, account, heroId }) {
+  const resource = `d3/profile/${account}/hero/${heroId}/items`
+  const API_URL = `${protocol}${region}${host}${resource}`
+  const locale = locales[region]
+
+  const params = {
+    'access_token': store.state.oauth.accessToken,
+    locale
+  }
+
+  return get(`${API_URL}`, { params })
+}
+
 export {
-  getApiAccount
+  getApiAccount,
+  getApiHero,
+  getApiDetailedHeroItems
 }
