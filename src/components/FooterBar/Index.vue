02514cd35b93912d0e3be42b84a6e727018912b5
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-24 19:07:51 +0100

    Update Index.vue

--- src/components/FooterBar/Index.vue
+++ src/components/FooterBar/Index.vue
@@ -1,9 +1,10 @@
 <template>
   <footer class="footer-bar text-center mt-5">
     <p class="text-muted">Powered by</p>
+
     <ul class="list-inline">
       <li class="list-inline-item mx-2" v-for="(icon, idx) in icons" :key="idx">
-        <a :href="icon.href" target="_blank">
+        <a :href="icon.href" target="_blank" :title="icon.href">
           <font-awesome-icon :icon="icon.classes" class="fa-lg" :style="{'color': icon.color}"/>
         </a>
       </li>
@@ -39,14 +40,18 @@ export default {
           href: 'https://bootstrap-vue.js.org/'
         },
         {
-          classes: [ 'fab', 'font-awesome' ],
-          color: '#0e95ff',
-          href: 'https://fontawesome.com/'
+          classes: [ 'fab', 'battle-net' ],
+          href: 'https://develop.battle.net/documentation/diablo-3/community-apis'
         },
         {
           classes: [ 'fab', 'github' ],
           color: '#e4e4e4',
           href: 'https://github.com/baumannzone'
+        },
+        {
+          classes: [ 'fab', 'font-awesome' ],
+          color: '#0e95ff',
+          href: 'https://fontawesome.com/'
         }
       ]
     }
