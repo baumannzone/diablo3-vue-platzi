c25f5fbf83c507654c17451d8dee2ddf0dd112c6
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-24 18:35:37 +0100

    Update BrandTitle.vue

--- src/components/HeaderBar/BrandTitle.vue
+++ src/components/HeaderBar/BrandTitle.vue
@@ -1,6 +1,7 @@
 <template>
   <b-navbar-brand :to="{ name: 'Home' }" class="hover-cursor-pointer">
     <img src="@/assets/img/diablo-iii.svg" alt="D3" width="30">
+    <span class="font-diablo ml-2">D3PF</span>
   </b-navbar-brand>
 </template>
 
