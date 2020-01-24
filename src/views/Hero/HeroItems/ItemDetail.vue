0655e56fea7f85fb018b4e27b78de0aca1bf2b7b
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-24 16:26:36 +0100

    Update ItemDetail.vue

--- src/views/Hero/HeroItems/ItemDetail.vue
+++ src/views/Hero/HeroItems/ItemDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center bg-dark h-100 pt-3">
+  <div class="text-center bg-dark h-100 pt-3 d3-icon-item" :class="itemClassColor">
 
     <div class="d-flex flex-column justify-content-between h-100">
       <div>
@@ -11,7 +11,7 @@
 
       <div>
         <template v-if="itemHasGems">
-          <small >Gems:</small>
+          <small>{{ gemOrJewel }}:</small>
           <ul class="list-inline">
             <li v-for="(gem, index) in item.gems" :key="index" class="list-inline-item">
               <GemItem :gem="gem"/>
@@ -44,7 +44,35 @@ export default {
     },
     itemHasGems () {
       return this.item.hasOwnProperty('gems')
+    },
+    gemOrJewel () {
+      return this.item.gems[0].isGem ? 'Gems' : 'Jewel'
+    },
+    itemClassColor () {
+      return `item-${this.item.displayColor}`
     }
   }
 }
 </script>
+
+<style lang="stylus">
+  .d3-icon-item
+    border-top-style solid
+    border-top-width 4px
+
+    &.item-green
+      border-color #8bc34a
+
+    &.item-orange
+      border-color #ff9800
+
+    &.item-yellow
+      border-color #ffeb3b
+
+    &.item-blue
+      border-color #03a9f4
+
+    &.item-white
+      border-color #a0aab5
+
+</style>
