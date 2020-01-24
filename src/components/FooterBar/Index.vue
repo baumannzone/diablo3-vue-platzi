e96c398bd079379512bec1a64e93a484966d8d5f
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-24 11:33:35 +0100

    Update Index.vue

--- src/components/FooterBar/Index.vue
+++ src/components/FooterBar/Index.vue
@@ -1,8 +1,8 @@
 <template>
-  <footer class="footer-bar text-center">
+  <footer class="footer-bar text-center mt-5">
     <p class="text-muted">Powered by</p>
     <ul class="list-inline">
-      <li class="list-inline-item" v-for="(icon, idx) in icons" :key="idx">
+      <li class="list-inline-item mx-2" v-for="(icon, idx) in icons" :key="idx">
         <a :href="icon.href" target="_blank">
           <font-awesome-icon :icon="icon.classes" class="fa-lg" :style="{'color': icon.color}"/>
         </a>
@@ -37,6 +37,16 @@ export default {
           classes: [ 'fab', 'bootstrap' ],
           color: '#7952b3',
           href: 'https://bootstrap-vue.js.org/'
+        },
+        {
+          classes: [ 'fab', 'font-awesome' ],
+          color: '#0e95ff',
+          href: 'https://fontawesome.com/'
+        },
+        {
+          classes: [ 'fab', 'github' ],
+          color: '#e4e4e4',
+          href: 'https://github.com/baumannzone'
         }
       ]
     }
