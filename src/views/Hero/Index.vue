bfa762e133b204aa10bfaaa5709c2d02189861df
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-26 00:44:34 +0100

    Update Index.vue

--- src/views/Hero/Index.vue
+++ src/views/Hero/Index.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="hero-view">
 
-    <template v-if="hero">
+    <BaseLoading v-if="isLoadingHero"/>
 
-      <HeroDetailHeader :detail="detailHeader"/>
+    <HeroDetailHeader v-if="hero" :detail="detailHeader"/>
 
-      <b-row>
-        <b-col md="12" lg="8" order-lg="2">
-          <HeroItems :items="items"/>
-        </b-col>
+    <b-row>
+      <b-col md="12" lg="8" order-lg="2">
+        <BaseLoading v-if="isLoadingItems"/>
+        <HeroItems v-if="items" :items="items"/>
+      </b-col>
 
-        <b-col md="12" lg="4" order-lg="1">
+      <b-col md="12" lg="4" order-lg="1">
+        <template v-if="hero">
           <GearBonuses :stats="detailStats"/>
           <Skills :skills="hero.skills"/>
-        </b-col>
+        </template>
+      </b-col>
 
-      </b-row>
-
-    </template>
+    </b-row>
 
   </div>
 </template>
 
 <script>
+import BaseLoading from '@/components/BaseLoading'
 import { getApiHero, getApiDetailedHeroItems } from '@/api/search'
 
 import HeroDetailHeader from './HeroDetailHeader'
@@ -32,16 +34,29 @@ import Skills from './Skills/Index'
 
 export default {
   name: 'HeroView',
-  components: { Skills, GearBonuses, HeroDetailHeader, HeroItems },
+  components: { BaseLoading, Skills, GearBonuses, HeroDetailHeader, HeroItems },
   data () {
     return {
+      isLoadingHero: false,
+      isLoadingItems: false,
       hero: null,
       items: null
     }
   },
   computed: {
     detailHeader () {
-      const { name, class: classSlug, gender, level, hardcore, seasonal, paragonLevel, alive, seasonCreated } = this.hero
+      const {
+        name,
+        class: classSlug,
+        gender,
+        level,
+        hardcore,
+        seasonal,
+        paragonLevel,
+        alive,
+        seasonCreated
+      } = this.hero
+
       return {
         name,
         classSlug,
@@ -59,26 +74,32 @@ export default {
     }
   },
   created () {
+    this.isLoadingHero = true
+    this.isLoadingItems = true
     const { region, battleTag: account, heroId } = this.$route.params
 
     getApiHero({ region, account, heroId })
       .then(({ data }) => {
-        console.log(data)
         this.hero = data
       })
       .catch((err) => {
         this.hero = null
         console.log(err)
       })
+      .finally(() => {
+        this.isLoadingHero = false
+      })
 
     getApiDetailedHeroItems({ region, account, heroId })
       .then(({ data }) => {
-        console.log(data)
         this.items = data
       })
       .catch((err) => {
         this.items = null
         console.log(err)
+      })
+      .finally(() => {
+        this.isLoadingItems = false
       })
   }
 }
