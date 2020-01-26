9b94429e8651e4b7579e909c0ed5821a9e56807b
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-26 13:24:31 +0100

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import Vuex from 'vuex'
 
 import oauth from './modules/oauth'
 import loading from './modules/loading'
+import error from './modules/error'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     oauth,
-    loading
+    loading,
+    error
   }
 })
