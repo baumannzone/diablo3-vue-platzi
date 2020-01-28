52e82a45ee3aca336b5f26870844e8d694c8ecbe
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-29 00:03:18 +0100

    Update Index.spec.js

--- tests/unit/src/components/FooterBar/Index.spec.js
+++ tests/unit/src/components/FooterBar/Index.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils'
-import IndexFB from '@/components/FooterBar/Index.vue'
+import Index from '@/components/FooterBar/Index.vue'
 import MadeByFoot from '@/components/FooterBar/MadeByFoot'
 import PoweredByFoot from '@/components/FooterBar/PoweredByFoot'
 import FootLinks from '@/components/FooterBar/FootLinks'
@@ -7,23 +7,20 @@ import FootLinks from '@/components/FooterBar/FootLinks'
 describe('FooterBar', () => {
   describe('Index.vue', () => {
     it('MadeByFoot component exists', () => {
-      const wrapper = shallowMount(IndexFB, {})
+      const wrapper = shallowMount(Index, {})
 
-      // console.log(wrapper.vm)
       expect(wrapper.find(MadeByFoot).exists()).toBe(true)
     })
 
     it('PoweredByFoot component exists', () => {
-      const wrapper = shallowMount(IndexFB, {})
+      const wrapper = shallowMount(Index, {})
 
-      // console.log(wrapper.vm)
       expect(wrapper.find(PoweredByFoot).exists()).toBe(true)
     })
 
     it('FootLinks component exists', () => {
-      const wrapper = shallowMount(IndexFB, {})
+      const wrapper = shallowMount(Index, {})
 
-      // console.log(wrapper.vm)
       expect(wrapper.find(FootLinks).exists()).toBe(true)
     })
   })
