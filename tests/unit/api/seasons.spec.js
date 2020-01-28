04ccd1b0a1d21a09d56d913166cc1cc6d65a3f39
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-28 22:19:20 +0100

    Create seasons.spec.js
    List Season

---
+++ tests/unit/api/seasons.spec.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import store from '@/store/index'
+import { listSeasons } from '@/api/season'
+
+jest.mock('axios')
+
+describe('API / search.js', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('listSeasons', async () => {
+    const token = 'EUNj8xPOH5DBzsuvleFjg3omsEX9wtmO0T'
+    const region = 'eu'
+    const url = `https://${region}.api.blizzard.com/data/d3/season/`
+
+    const params = {
+      params: {
+        access_token: token
+      }
+    }
+
+    store.state.oauth.accessToken = token
+
+    await expect(listSeasons(region))
+
+    expect(axios.get).toBeCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledWith(url, params)
+  })
+})
