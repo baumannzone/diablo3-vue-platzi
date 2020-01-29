0da1d2b985aa9afd5570c75164d7b109f7e21bcf
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-01-30 00:44:11 +0100

    Update BrandTitle.vue

--- src/components/HeaderBar/BrandTitle.vue
+++ src/components/HeaderBar/BrandTitle.vue
@@ -1,12 +1,19 @@
 <template>
   <b-navbar-brand :to="{ name: 'Home' }" class="hover-cursor-pointer">
     <img src="@/assets/img/diablo-iii.svg" alt="D3" width="30">
-    <span class="font-diablo ml-2">D3PF</span>
+    <span class="font-diablo ml-2">{{ title }}</span>
   </b-navbar-brand>
 </template>
 
 <script>
 export default {
-  name: 'BrandTitle'
+  name: 'BrandTitle',
+  data () {
+    return {
+      // 🔥 Note that only variables that start with VUE_APP_ will be statically embedded
+      // into the client bundle with webpack.DefinePlugin.
+      title: process.env.VUE_APP_TITLE
+    }
+  }
 }
 </script>
