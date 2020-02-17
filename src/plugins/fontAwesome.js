a53ea8eb7f01013de97b83bf8ea987bb5fb608ac
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-02-17 11:25:50 +0100

    Remove Default Logo :v:

--- src/plugins/fontAwesome.js
+++ src/plugins/fontAwesome.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch, faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
 import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(
-  faSearch,
   faSkull,
   faCrown,
   faDungeon,
