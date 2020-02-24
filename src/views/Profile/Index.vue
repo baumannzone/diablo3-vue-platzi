f29466bb9354c57bf9258ff8e4f1847366b58cf9
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-02-24 21:07:15 +0100

    Update Index.vue

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -48,27 +48,37 @@ export default {
   created () {
     this.isLoading = true
     const { region, battleTag: account } = this.$route.params
-    // Fetch Data
-    getApiAccount({ region, account })
-      .then(({ data }) => {
-        this.profileData = data
-      })
-      .catch((err) => {
-        this.profileData = null
-        const errObj = {
-          routeParams: this.$route.params,
-          message: err.message
-        }
-        if (err.response) {
-          errObj.data = err.response.data
-          errObj.status = err.response.status
-        }
-        this.setApiErr(errObj)
-        this.$router.push({ name: 'Error' })
-      })
-      .finally(() => {
-        this.isLoading = false
-      })
+    this.fetchData(region, account)
+  },
+  methods: {
+    /**
+     * Fetch data
+     * @param region {String}
+     * @param account {String}
+     */
+    fetchData (region, account) {
+      // Fetch Data
+      getApiAccount({ region, account })
+        .then(({ data }) => {
+          this.profileData = data
+        })
+        .catch((err) => {
+          this.profileData = null
+          const errObj = {
+            routeParams: this.$route.params,
+            message: err.message
+          }
+          if (err.response) {
+            errObj.data = err.response.data
+            errObj.status = err.response.status
+          }
+          this.setApiErr(errObj)
+          this.$router.push({ name: 'Error' })
+        })
+        .finally(() => {
+          this.isLoading = false
+        })
+    }
   }
 }
 </script>
